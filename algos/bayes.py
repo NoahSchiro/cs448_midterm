b0a1134b1e85911723e23eee89d1cb82f45cd337
NoahSchiro <noahschiro@rocketmail.com>
2023-09-15 11:39:14 -0400

    inital work on bayes

--- algos/bayes.py
+++ algos/bayes.py
@@ -1,15 +1,24 @@
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
 
 class Bayes():
 
     # Intialize model
     def __init__(self):
-        pass
+        self.model = GaussianNB()
 
     # Training the model
-    def train(self, data):
-        pass
+    def train(self, X_train, Y_train):
+        self.model.fit(X_train, Y_train)
+
+    def test(self, X_test, Y_test):
+
+        # making predictions on the testing set
+        Y_pred = self.model.predict(X_test)
+          
+        # comparing actual response values (y_test) with predicted response values (y_pred)
+        print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(Y_test, Y_pred)*100)
 
     # Forward is like our inference
     def forward(self, x):
-        pass
+        return self.model.predict(x)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from algos.bayes import Bayes
 
+word_mapping = {}
+pos_mapping  = {}
 
 def get_data():
     with open("train.txt", "r") as f:
@@ -18,8 +20,49 @@ def get_data():
 
         return complete_data
 
+def data_preprocess(data):
+
+    # Filter out all training pieces that don't have 2 elements
+    data = list(filter(lambda x: len(x) >= 2, data))
+
+    # The data is composed as [(X_2, Y_2), (X_2, Y_2), ...]
+    # So we will begin by splitting this into [[X_1], [X_2], ...]] and [Y_1, Y_2, ...]
+
+    X = list(map(lambda x: x[0], data))
+    Y = list(map(lambda x: x[1], data))
+    print(X)
+
+    return X, Y
+
+def data_vectorize(data):
+
+    word_counter = 0
+
+    for (X, Y, _) in data:
+        if X not in word_mapping:
+            word_mapping[X] = word_counter
+            word_counter += 1
+
+
 if __name__=="__main__":
+
+    # Retrieve data
     data = get_data()
 
+    # Calculate mappings
+    data_vectorize(data) 
+
+    # Perform a train / validate split of 80/20
+    idx = int(len(data) * 0.8)
+    train_ds, val_ds = data[:idx], data[idx:]
+
+    X_train, Y_train = data_preprocess(train_ds)
+    X_val, Y_val = data_preprocess(val_ds)
+
+    # Intialize models
     b = Bayes()
+    # s = SVM()
+    # r = Regression()
+
+    b.train(data)
 
