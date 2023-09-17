ae9f32cfc7d42abc1baf99268ed1a6f90e22fc25
Kofwana <kofwanlawson@gmail.com>
2023-09-17 19:17:01 -0400

    moved reusable modules to pos_utils file

---
+++ pos_utils.py
@@ -0,0 +1,40 @@
+# pos_utils.py
+
+
+# We only need the first two columns (token and POS tag),
+# modifying the code slightly to return only those columns.
+# get_data() reads and parses the training data from 'train.txt' and returns a list of tuples
+# containing token and POS tag.
+
+def get_data():
+    with open("train.txt", "r") as f:
+        lines = f.read().strip().split('\n')
+        data = [line.split(' ')[:2] for line in lines]
+    return data
+
+# Feature extraction: defining features for each token based on its context,
+# such as the previous and next words, prefixes, suffixes, etc. 
+# extract_features() extracts features for a given token in a sentence. In this example, it uses the
+# current token as a feature.
+
+def extract_features(token):
+    #token = sentence[index][0]
+    return {'token': token}
+
+# Prepare Training Data:  creating feature vectors and corresponding
+# labels (POS tags) for logistic regression model using 
+# import nltk
+# prepare_data() Prepares the training data by extracting features and labels (POS tags) for
+# the Logistic Regression model.
+
+def prepare_data(data):
+    features = []
+    labels = []
+
+    for sentence in data:
+        token = sentence[0]  # Assuming the token is the first (and only) element
+        pos_tag = sentence[1] if len(sentence) > 1 else ''  # Use the second element if available, or an empty string if not
+        features.append(extract_features(token))
+        labels.append(pos_tag)
+
+    return features, labels
