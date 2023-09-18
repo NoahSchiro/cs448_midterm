62ef95895a223be7b1503ba9a9307e4ba3a31939
NoahSchiro <noahschiro@rocketmail.com>
2023-09-18 11:17:19 -0400

    nitpicky changes

--- algos/utils.py
+++ algos/utils.py
@@ -1,5 +1,4 @@
-# tip for code to work: run in term "pip3 install scikit-learn" and " pip3 install nltk"
-# need to install scikit-learn (type "pip install scikit-learn" in terminal)
+# tip for code to work: run in term "pip3 install scikit-learn"
 
 # Import for generating classification report
 from sklearn.metrics import classification_report
@@ -8,13 +7,14 @@ from sklearn.metrics import classification_report
 # modifying the code slightly to return only those columns.
 # get_data() reads and parses the training data from 'train.txt' and returns a list of tuples
 # containing token and POS tag.
+
 def get_data():
     with open("train.txt", "r") as f:
         lines = f.read().strip().split('\n')
         data = [line.split(' ')[:2] for line in lines]
     return data
 
-# Featue extraction: defining features for each token based on its context,
+# Feature extraction: defining features for each token based on its context,
 # such as the previous and next words, prefixes, suffixes, etc. 
 # extract_features() extracts features for a given token in a sentence. In this example, it uses the
 # current token as a feature.
@@ -23,21 +23,15 @@ def extract_features(token):
     #token = sentence[index][0]
     return {'token': token}
 
-# debugging: In this updated function,
-# we assume that sentence is a list of tuples, 
-# and we access the token using sentence[index][0]. 
-# This should resolve the TypeError issue and yet........
-
-
 # Prepare Training Data:  creating feature vectors and corresponding
 # labels (POS tags) for logistic regression model using 
 # import nltk
 # prepare_data() Prepares the training data by extracting features and labels (POS tags) for
 # the Logistic Regression model.
+
 def prepare_data(data):
     features = []
     labels = []
-
 
     for sentence in data:
         token = sentence[0]  # Assuming the token is the first (and only) element
@@ -47,31 +41,8 @@ def prepare_data(data):
 
     return features, labels
 
-
-# Evaluation using cross validation or not...
-# evaluate_model() evaluates the trained model using a small part 
-# of the training data as a dev set and prints the classification report.
-def evaluate_model(model, vectorizer, data):
-    dev_features, dev_labels = prepare_data(data)
-    X_dev = vectorizer.transform(dev_features)
-    predicted_labels = model.predict(X_dev)
-    
-    print(classification_report(dev_labels, predicted_labels))
-
 if __name__ == "__main__":
 
-<<<<<<< HEAD:algos/utils.py
-    data = get_data()
-
-=======
-def predict_pos_tags(sentence, vectorizer, clf):
-    features = [extract_features(token) for token in sentence]
-    X = vectorizer.transform(features)
-    predicted_labels = clf.predict(X)
-    return predicted_labels
-
-
-def main():
     # Load and preprocess the data
     data = get_data()
 
@@ -82,7 +53,6 @@ def main():
 
 
     # Prepare the data
->>>>>>> c5f2df5 ( debug: inner list contains one element (fixed)):Logistic_Reg.py
     features, labels = prepare_data(data)
 
     print(features[0])
--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-# This function read and parse the training data from "train.txt."
-# It returns a list of tuples where each tuple contains the token, 
-# POS tag, and Chunking tag. 
-def get_data():
-    with open("train.txt", "r") as f:
-        # Long string
-        txt = f.read()
-
-        # List of strings
-        lines = txt.split('\n')
-
-        complete_data = []
-
-        for line in lines:
-            # Split each line into a 3 tuple
-            complete_data.append(tuple(line.split(' ')))
-
-        return complete_data
+import algos.Logistic_Reg as reg
+from algos.utils import get_data
 
 if __name__=="__main__":
+
+    # Get the data
     data = get_data()
 
+    # Run the regression script
+    reg.main(data)
+
+    #bayes.main(data)
+
+    #svm.main(data)
 
